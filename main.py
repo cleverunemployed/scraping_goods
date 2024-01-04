d19cb763ea74a604209741b71cbd33d8dcbc10b6
cleverunemployed <154885944+cleverunemployed@users.noreply.github.com>
2024-01-04 10:57:03 +0400

    Update main.py
    на 4 ссылках отработал нормально, но с искажением

--- main.py
+++ main.py
@@ -1,128 +1,232 @@
 import time
-# import asyncio
-# import aiohttp
-# import aiofiles
 import csv
+import sched
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from bs4 import BeautifulSoup
 from multiprocessing import Pool
 
 
-
-url_main: str = "https://moba.ru/catalog/"
-COUNT: int = 0
-
-# Получение исходной страницы
-def get_page(url: str) -> None:
-    option = webdriver.EdgeOptions()
-    option.add_argument("--headless")
-    driver = webdriver.Edge(options=option)
-    driver.get(url)
-    time.sleep(5)
-    with open('text.txt', 'w', encoding='utf-8') as file:
-        file.write(driver.page_source)
-    driver.close()
+# URL для сбора ссылок на категории товаров
+url_1: str = "https://omsk.moba.ru/catalog/akkumulyatory-1/filter/imyie_stores4filter-is-omsk/apply/"
+url_2: str = "https://omsk.moba.ru/catalog/korpusa-zadnie-kryshki/filter/imyie_stores4filter-is-omsk/apply/"
+url_3: str = "https://omsk.moba.ru/catalog/shleyfy-platy/filter/imyie_stores4filter-is-omsk/apply/"
+url_4: str = "https://omsk.moba.ru/catalog/displei/filter/imyie_stores4filter-is-omsk/apply/"
+# Функция для сохранение страницы с каталогами
+urls: tuple = (url_1, url_2, url_3, url_4)
 
 # Сбор ссылок на каталоги
 def find_links() -> None:
+    # Открываем файл с содержанием страницы от каталогов
     with open('text.txt', 'r', encoding='utf-8') as file:
         soup = file.read()
+    # Создаём объект "Супа"
     soup = BeautifulSoup(soup, 'lxml')
+    # Сбор ссылок на каталоги
     block = soup.find('ul', class_="left_menu").find_all('li')
+    # Запись в файл links ссылок на каталоги
     with open("links", "a+", encoding='utf-8') as file:
+        # Проход по каталогам
         for i in block:
+            # Проверка ссылки на "ссылку ребёнка"
             if str(i).count("menu_item") == 1 or str(i).count("child_container") == 0:
-                print(i.find('a')['href'])
-                file.write("https://moba.ru" + i.find('a')['href'] + '\n')
+                # Записываем ссылку в файл
+                file.write("https://omsk.moba.ru" + i.find('a')['href'] + '\n')
+
 
 # Сбор информации о товаре
-def find_articles_and_amount(url: str) -> list:
-    option = webdriver.EdgeOptions()
-    option.add_argument("--headless")
+def find_articles_and_amount(url: str) -> None:
 
-    driver = webdriver.Edge(options=option)
-    driver.get(url)
+    # Опции для без экранного захода на сайт
+    option = webdriver.ChromeOptions()
+    #option.add_argument("--window-size=10,10")
+    option.add_argument('--headless')
 
+
+
+    # Посылаем GET запрос на сайт
+    driver = webdriver.Chrome(options=option)
+    driver.get(f'{url}?cid=14411')
+    #time.sleep(2)
+    # Создаём объект "Супа"
     soup = BeautifulSoup(str(driver.page_source), 'lxml')
-
-    article = soup.find('div', class_='article iblock').find('span', 'value').text
-    name = soup.find('h1', id='pagetitle').text
-    first_price = soup.find('div', class_='cost prices clearfix').find_all('span', class_='price_value')[0].text
-    second_price = soup.find('div', class_='cost prices clearfix').find_all('span', class_='price_value')[1].text
+    # "Отлов" ошибок при сборе информации о товаре
     try:
-        button = driver.find_element(By.CLASS_NAME, "right_info").find_element(By.CLASS_NAME, "buy_block").find_element(By.CLASS_NAME, "counter_wrapp").find_element(By.TAG_NAME, "input")
-        button.clear()
+
+        button = driver.find_element(By.ID, f'bx_117848907_{url.split("/")[-2]}_quantity')
+        button.click()
         button.send_keys("51")
         button.click()
-        amount = soup.find('span', class_="btn-lg w_icons to-cart btn btn-default transition_bg animate-load")[
-            "data-quantity"]
-        status = 'в наличии'
+        button.send_keys(Keys.ENTER)
+        #time.sleep(2)
+        amount = driver.find_element(By.ID, f"bx_117848907_{url.split('/')[-2]}_basket_actions").find_element(By.TAG_NAME, "span")
+
+        amount = str(amount.get_attribute("data-quantity"))
+        # Сбор артикула
+        article = soup.find('div', class_='article iblock').find('span', class_='value').text
+        # Сбор названия
+        name = soup.find('h1', id='pagetitle').text.replace(',', ' ')
+        # Сбор первой цены
+        first_price = soup.find('div', class_='cost prices clearfix').find_all('span', class_='price_value')[0].text
+        # Сбор второй цены
+        second_price = soup.find('div', class_='cost prices clearfix').find_all('span', class_='price_value')[1].text
     except:
-        amount = 0
-        status = 'под заказ'
-    print([name, article, first_price, second_price, amount, status])
-    return [name, article, first_price, second_price, amount, status]
+        # При ошибке сбора информации переходим к следующей ссылке
+        return
+    # "Отлов" ошибки при сборе информации о наличии товара
+    try:
+        # Сбор информации о наличии товара
+        status = soup.find('div', class_='quantity_block_wrapper').find('span', class_='item_instock value').text
+    except:
+        # При ошибке, нет в наличии
+        status = 'Под заказ'
+
+    # Открываем csv файл для добавлении информации о товаре
+    with open('table.csv', 'a', newline='') as table:
+        csvrow = csv.writer(table)
+        # Записываем информацию о товаре в строку
+        csvrow.writerow([name, article, first_price, second_price, amount, status])
+
 
 # Сбор ссылок на товары
 def find_data(url: str) -> None:
 
-    global COUNT
-
+    # Опции для без экранного захода на сайт
     option = webdriver.EdgeOptions()
     option.add_argument("--headless")
 
+    # Посылаем GET запрос на сайт
     driver = webdriver.Edge(options=option)
     driver.get(url)
 
-    with open('table.txt', 'a', encoding='utf-8') as table:
+    # Открываем файл для записи в него ссылок на товары
 
-        while True:
+    with open('table.txt', 'a+', encoding='utf-8') as table:
 
-            soup = BeautifulSoup(str(driver.page_source), 'lxml')
-            blocks = soup.find_all('td', class_="wrapper_td")
+        # Создаём объект "Супа"
 
-            for i in blocks:
-                link = i.find('a', class_='dark_link')['href']
-                COUNT += 1
-                table.write(f"https://moba.ru{link}\n")
-            try:
-                driver.find_element(By.CLASS_NAME, "flex-next").click()
-                print(f"okey {link}")
-            except:
-                break
-    print(f'[+] {COUNT}')
-    driver.close()
+        soup = BeautifulSoup(str(driver.page_source), 'lxml')
 
-# Запуск всей программы
-def main():
-    first_main()
-    #second_main()
+        # Поиск "блоков" с ссылкой на товар
+
+        blocks = soup.find_all('td', class_="wrapper_td")
+
+        for i in blocks:
+
+            # Поиск ссылки на товар
+
+            link = i.find('a', class_='dark_link')['href']
+
+            # Подготовляем ссылку
+
+            word = f"https://omsk.moba.ru{link}\n"
+
+            # Запись ссылки в файл
+
+            table.write(word)
+
+        # Закрываем "запрос"
+
+        driver.close()
+
+
+# Поиск количества страниц в каталоге на товар
+def pagination_find(link: str):
+
+    # Опции для без экранного захода на сайт
+    option = webdriver.EdgeOptions()
+    option.add_argument("--headless")
+
+    # Посылаем GET запрос на сайт
+    driver = webdriver.Edge(options=option)
+    driver.get(link)
+
+    # Создаём объект "Супа"
+    soup = BeautifulSoup(str(driver.page_source), 'lxml')
+
+    # Проверка: имеются ли вообще страницы в каталоге
+    try:
+        # Путь к "пагинации"
+        page = int(soup.find_all('a', class_='dark_link')[-1].text)
+    except:
+        return 0
+    links: list = []
+    # Проход по страницам
+    for i in range(1, page + 1):
+        # Кидаем ссылку на сбор с неё информацию
+        links.append(f'{link}?PAGEN_1={i}')
+
+    pool2 = Pool(5)
+    pool2.map(find_data, links)
+
 
 # Запуск первой части программы
 def first_main():
-    # get_page(url_main)
-    # find_links()
-    pool = Pool(10)
-    with open('links', 'r', encoding='utf-8') as file:
-       links = list(map(lambda x: x[0:-2], file.readlines()))
-    pool.map(find_data, links)
+    # get_page(url_main) - сохранение страницы с каталогами
+    # find_links() - извлечение ссылок из каталогов
 
-    print(f"Finished {len(links)}")
+    # Создаём "бассейн" процессов их количество равно 10 можно поставить сколько вам угодно
+    pool = Pool(10)
+
+    # Записываем в список links ссылки на каталоги
+    with open('links', 'r', encoding='utf-8') as file:
+        # Преобразуем данные из файла в список ссылок
+        links = list(map(lambda x: x[0:-2], file.readlines()))
+
+    # Многопоточная работа с ссылками
+    pool.map(pagination_find, links)
+
+    # Обработка последней ссылки
+    pagination_find(links[-1])
+
 
 # Запуск второй части программы
 def second_main():
+
+    # Извлекаем ссылки из файла
     with open('table.txt', 'r') as table:
         text = table.readlines()
+
+    # Запись "шапки" в таблицу
     with open('table.csv', 'a', newline='') as table:
         csvrow = csv.writer(table)
-        csvrow.writerow(['Название', 'Артикул', 'Первая Цена', 'Вторая Цена', 'Количество', 'В наличии'])
-        for i in text:
-            p = i.replace('\n', '').replace('//', '/').replace('/:', '//:')
-            res = find_articles_and_amount(p)
-            csvrow.writerow(res)
-            print(f"[+] Success complete link {text.index(i)}/{len(text)}")
+        # "Шапка" таблицы
+        csvrow.writerow(['Название', 'Артикул', 'Первая Цена', 'Вторая Цена', 'Количество','В наличии'])
+
+    # Список ссылок
+    result: list = []
+
+    # Заполняем список ссылками
+    for i in text:
+        # Подготавливаем ссылку
+        p = i.replace('\n', '')
+        # Добавляем ссылку
+        result.append(p)
+
+    # Создаём "бассейн" процессов их количество равно 10 можно поставить сколько вам угодно
+    pool = Pool(10)
+    # Многопоточная работа с ссылками
+    pool.map(find_articles_and_amount, result)
 
 
+
+# Запуск всей программы (Точка входа)
+def main():
+    # Сбор ссылок на товары и запись их в файл
+    #first_main()
+    # Сбор информации о товаре и запись их в таблицу
+    #second_main()
+
+    #for i in urls:
+    #    pagination_find(i)
+
+    second_main()
+
+
+# Запуск скрипта
 if __name__ == "__main__":
     main()
